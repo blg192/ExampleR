fbf8b81a230a3bdcfca6e691d373d3c48179f9e1
Ben Graves <blgpp5@mail.missouri.edu>
2021-10-19 20:47:13 -0500

    adds contingency tables

--- BasicR/RM1GetStarted.R
+++ BasicR/RM1GetStarted.R
@@ -1,15 +1,13 @@
-## ----setup, include=FALSE--------------------------------------------------------------------------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE)
+## ----setup--------------------------------------------------------------------------------------------------------------------
 library(psych)
 library(psychTools)
 library(ggplot2)
 ## possible data
-data("mtcars")
-data("sleep")
-data("sat.act") ## in psych package
-data("epi.bfi") ## psychTools package
+## data("sleep")
+## data("sat.act") ## in psych package
 
 
+## ----Set WD------------------------------------------------------------------------------------------------------------------------
 ## ## Code to set the working directory
 ## 
 ## ## NOTE: This file path will be different for everyone and dependent on how
@@ -22,12 +20,12 @@ data("epi.bfi") ## psychTools package
 ## getwd()
 
 
-## Installing Packages
+## ----Install Packages---------------------------------------------------------------------------------------------------------------------
 ## ## Replace 'Package Name' with the name of the package you want to install
 ## install.packages("Package Name", dependencies = TRUE)
 
 
-## Frequency Distribution Section
+## ----Frequency Tables -----------------------------------------------------------------------------------------------------------------
 ## Load in psych library
 library(psych)
 ## Call up data set
@@ -42,53 +40,64 @@ sat.act$education <- factor(sat.act$education, levels = c(0,1,2,3,4,5),
 ## people are in each group of the gender variable in the data
 table(sat.act$gender)
 
+
+## ----Histogram--------------------------------------------------------------------------------------------------------------------
 ## The breaks argument can be used to increase or decrease the
 ## width of the bins used in the histogram
 hist(sat.act$ACT, breaks = 15, xlab = "ACT Score", main = "Histogram of ACT Scores")
 
 
+## ----Contingency Table---------------------------------------------------------------------------------------------------------------------
+## Frequency counts
+conttab <- table(sat.act$education, sat.act$gender)
+conttab
 
-## Measures of central tendency
-## Mean
+## Proportions
+prop.table(conttab)
+
+## Percentages
+prop.table(conttab)*100
+
+
+## ----Mean----------------------------------------------------------------------------------------------------------------------
 mean(sat.act$ACT)
 
-## Median
+
+## ----Median--------------------------------------------------------------------------------------------------------------------
 median(sat.act$ACT)
 
-## Mode
+
+## ----Mode------------------------------------------------------------------------------------
 library(modeest)
+
 mfv(sat.act$ACT)
 
-## Standard Deviation 
+
+## ----Standard Deviation------------------------------------------------------------------------------------------------------------------------
 sd(sat.act$ACT)
 
 
-
-
-## t-test Section
+## ----t-test---------------------------------------------------------------------------------------------------------------------
 data(sleep)
+
 t.test(extra ~ group, data = sleep)
 
-## NOTE: The defalut argument for t.test is to assume variances are not equal
+## NOTE: The default argument for t.test is to assume variances are not equal
 ## If you have tested and see they are, use var.equal = TRUE for Student's t-test
 
-## If data is is wide format, change the arguemtns to be two vectors.
+## If data is is wide format, change the arguments to be two vectors.
 ## t.test(sleep$group1, sleep$group2)
 
 
-## Cohen's D Section
-
+## ----Cohen's D -------------------------------------------------------------------------------------------------------------------
 library(MOTE)
+
 ## need t-statistic, group sizes, alpha level
 ## From sleep data above: t = -1.8608, group 1 n = 10, group 2 n = 10
 d.ind.t.t(t = -1.8608, n1 = 10, n2= 10, a = 0.05)
 
 
-
-
-
-## Plots Section
-## Bar Graph
+## ----Bar Graph---------------------------------------------------------------------------
 library(ggplot2)
 
 ## NOTE: Make sure your grouping variable is a 'factor'
@@ -113,6 +122,7 @@ sleepbar +
     ggtitle('Sleep Bar Graph') +cleanup
 
 
+## ----ACT Bar Graph-------------------------------------------------------------------------
 ## Another example using the ACT data
 actbar <- ggplot(data = sat.act, aes(education, ACT))
 
@@ -123,7 +133,7 @@ actbar +
     ggtitle('Average ACT Score Across Education Levels') +cleanup
 
 
-## Box Plot
+## ----Box Plot----------------------------------------------------------------------------
 sleepbox <- ggplot(sleep, aes(group, extra))
 
 sleepbox +
@@ -131,12 +141,12 @@ sleepbox +
     ggtitle('Box Plot for Sleep Data') + cleanup
 
 
-## Stem-and-Leaf Plot
+## ----Stem-and-Leaf----------------------------------------------------------------------------------------------------------------------
 ## The length of the plot can be changed with the 'scale = ' argument
 stem(sleep$extra)
 
 
-## Violin Plot
+## ----Violin Plot-----------------------------------------------------------------------------
 sleepviolin <- ggplot(sleep, aes(group, extra))
 
 sleepviolin +
@@ -144,18 +154,18 @@ sleepviolin +
     ggtitle('Box Plot for Sleep Data') + cleanup
 
 
-## Confidence Interval for a proportion 
+## ----Confidence Interval for Proportion --------------------------------------------------------------------------------------------------------------------
 ## This function requires the number of successes, 
 ## total number of observations, and probability to test against
 prop.test(x = 62, n = 100, p = .5)
 
 
-## Confidence Interval Plot
+## ----Confidence Interval Plot-------------------------------------------------------------------------------------------------------
 ## This code creates a loop to generate some data
 ## and then calculate the confidence interval
 lower <- rep(NA, 100)
 upper <- rep(NA, 100)
-set.seed(587463)
+set.seed(587500)
 for(i in 1:100) {
   y <- rnorm(100, mean = 5, sd = 5)
   lower[i] <- mean(y) - 1.96 * sd(y) / sqrt(length(y))
@@ -191,7 +201,7 @@ for(j in 1:100) {
 }
 
 
-## One-Way ANOVE Section
+## ----One-Way ANOVA-----------------------------------------------------------------------------------------------------------------------
 ## One-way ANOVA using lm() function 
 regmodel <- lm(ACT ~ education, data = sat.act)
 anova(regmodel)
@@ -212,7 +222,7 @@ ezANOVA(data = sat.act,
         return_aov = TRUE)
 
 
-## Post-hoc Section
+## ----Post-hoc -------------------------------------------------------------------------------------------------------------------
 ## The argument 'p.adjust.method = ' is what applies the correction
 ## in this case, we use a Bonferroni corrections on alpha
 pairwise.t.test(sat.act$ACT,
@@ -222,11 +232,11 @@ pairwise.t.test(sat.act$ACT,
                 p.adjust.method = 'bonferroni')
 
 
-## Two Predictor Regression Section
+## ----Two-Predictor Linear Regression----------------------------------------------------------------------------------------------------------------
 regmodel <- lm(ACT ~ SATV + SATQ, data = sat.act)
 summary(regmodel)
 
 
-## Confidence Interval for Regression
+## ----Regression Confidence Intervals-----------------------------------------------------------------------------------------------------------------------
 confint(regmodel, level = 0.95)
 
